c9b6dbb4dfc76d6ca64e36553019627ad39631f9
kahput <kahputs@gmail.com>
2025-05-23 14:37:05 +0200

    feat(network): create a simple client-server demo

--- src/client/main.c
+++ src/client/main.c
@@ -1,6 +1,47 @@
+#include <netdb.h>
+#include <stdint.h>
 #include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <errno.h>
 
-int main(void) {
-	printf("Hello from client!\n");
-	return 0;
+#define PORT "8080"
+
+int main() {
+	int32_t client_fd = 0;
+	struct addrinfo hints, *server_addr;
+	char message[1024] = { 0 };
+
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_INET6;
+	hints.ai_socktype = SOCK_STREAM;
+
+	int32_t status;
+	if ((status = getaddrinfo("localhost", PORT, &hints, &server_addr)) != 0) {
+		printf("getaddrinfo error: %s\n", gai_strerror(status));
+		freeaddrinfo(server_addr);
+		return -1;
+	}
+
+	if ((client_fd = socket(server_addr->ai_family, server_addr->ai_socktype, server_addr->ai_protocol)) < 0) {
+		printf("Failed to create socket\n");
+		freeaddrinfo(server_addr);
+		return -1;
+	}
+
+	if (connect(client_fd, server_addr->ai_addr, server_addr->ai_addrlen) < 0) {
+		printf("Connect error: %s\n", strerror(errno)); // Show actual error
+		printf("Failed to connect to server on port %s\n", PORT);
+		close(client_fd);
+		freeaddrinfo(server_addr);
+		return -1;
+	}
+
+	freeaddrinfo(server_addr);
+
+	read(client_fd, message, sizeof(message));
+	printf("Message from server: %s\n", message);
+
+	close(client_fd);
 }
--- src/server/main.c
+++ src/server/main.c
@@ -1,6 +1,61 @@
+#include <netdb.h>
+#include <stdint.h>
 #include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#define PORT "8080"
 
 int main() {
-	printf("Hello from server!\n");
+	int32_t server_fd, client_fd;
+	struct addrinfo hints, *server_addr;
+
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_INET6;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_flags = AI_PASSIVE;
+
+	int32_t status;
+	if ((status = getaddrinfo(NULL, PORT, &hints, &server_addr)) != 0) {
+		fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
+		return -1;
+	}
+
+	if ((server_fd = socket(server_addr->ai_family, server_addr->ai_socktype, server_addr->ai_protocol)) < 0) {
+		printf("Server failed to create socket\n");
+		close(server_fd);
+		freeaddrinfo(server_addr);
+		return -1;
+	}
+
+	if (bind(server_fd, server_addr->ai_addr, server_addr->ai_addrlen) < 0) {
+		printf("Server failed to bind on port %s\n", PORT);
+		close(server_fd);
+		freeaddrinfo(server_addr);
+		return -1;
+	}
+
+	if (listen(server_fd, 5) < 0) {
+		printf("Server failed to listen on port %s\n", PORT);
+		close(server_fd);
+		freeaddrinfo(server_addr);
+		return -1;
+	}
+
+	printf("Server listening on port %s...\n", PORT);
+
+	if ((client_fd = accept(server_fd, NULL, NULL)) < 0) {
+		printf("Server failed to accept connect on port %s\n", PORT);
+		close(server_fd);
+		freeaddrinfo(server_addr);
+		return -1;
+	}
+
+	char message[] = "Hello from server!";
+	send(client_fd, message, sizeof(message), 0);
+
+	close(server_fd);
+	close(client_fd);
 	return 0;
 }
