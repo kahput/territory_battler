3f345c5b53cb78ccbcdeb0801574c292b49ce534
kahput <kahputs@gmail.com>
2025-06-14 13:46:32 +0200

    feat: reset player movement timer after moving

--- src/client/main.c
+++ src/client/main.c
@@ -11,14 +11,13 @@
 
 typedef struct _player {
 	Vector2 position, size;
-	Vector2 direction, speed;
+	int direction, speed;
 } Player;
 
-void print_player(Player p) {
-	printf("Player {\n  position { x = %.2f, y = %.2f }\n  direction { x = %.2f, y = %.2f}\n}\n",
-		p.position.x, p.position.y,
-		p.direction.x, p.direction.y);
-}
+void draw_grid(uint32_t grid_target[GRID_SIZE * GRID_SIZE]);
+
+Vector2 to_vec2(int32_t direction);
+void print_player(Player p);
 
 int main() {
 	InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Land.io");
@@ -26,48 +25,56 @@ int main() {
 	Player player = {
 		.position = { 3 * GRID_SIZE, 3 * GRID_SIZE },
 		.size = { GRID_SIZE, GRID_SIZE },
-		.speed = { 128, 128 }
+		.speed = 128
 	};
 
 	float delta_time = 0.0f;
 	float last_frame = 0.0f;
 
-	Color colors[5] = { RAYWHITE, RED, BLUE, GREEN, ORANGE };
 	uint32_t grid_target[GRID_SIZE * GRID_SIZE] = { 0 };
+
+	float step_timer = 0;
 
 	while (!WindowShouldClose()) {
 		ClearBackground(RAYWHITE);
-
-		player.direction.x = IsKeyDown(KEY_D) - IsKeyDown(KEY_A);
-		player.direction.y = IsKeyDown(KEY_S) - IsKeyDown(KEY_W);
 
 		float current_frame = GetTime();
 		delta_time = current_frame - last_frame;
 		last_frame = current_frame;
 
-		player.direction = Vector2Normalize(player.direction);
-		player.position = Vector2Add(player.position, Vector2Multiply(Vector2Multiply(player.direction, player.speed), (Vector2){ delta_time, delta_time }));
+		step_timer += player.speed * delta_time;
+
+		if (step_timer >= 32 && player.direction != 0) {
+			player.position = Vector2Add(player.position, Vector2Scale(to_vec2(player.direction), GRID_SIZE));
+			step_timer = 0;
+		}
+
+		int32_t new_direction = 0;
+		if (IsKeyDown(KEY_W))
+			new_direction = 1;
+		if (IsKeyDown(KEY_D))
+			new_direction = 2;
+		if (IsKeyDown(KEY_S))
+			new_direction = 3;
+		if (IsKeyDown(KEY_A))
+			new_direction = 4;
+
+		if (new_direction != 0 && player.direction != new_direction) {
+			step_timer = 0;
+			player.direction = new_direction;
+		}
 
 		BeginDrawing();
 
-		for (uint32_t y = 0; y < WINDOW_HEIGHT / GRID_SIZE; y++) {
-			for (uint32_t x = 0; x < WINDOW_WIDTH / GRID_SIZE; x++) {
-				uint32_t index = x + y * (WINDOW_WIDTH / GRID_SIZE);
-				DrawRectangle(x * GRID_SIZE, y * GRID_SIZE, GRID_SIZE, GRID_SIZE, colors[grid_target[index]]);
-				DrawRectangleLines(x * GRID_SIZE, y * GRID_SIZE, GRID_SIZE, GRID_SIZE, GRAY);
-			}
-		}
+		draw_grid(grid_target);
 
-		int32_t x = roundf(player.position.x / (float)GRID_SIZE) * GRID_SIZE;
-		int32_t y = roundf(player.position.y / (float)GRID_SIZE) * GRID_SIZE;
-		uint32_t index = (x / GRID_SIZE) + (y / GRID_SIZE) * (WINDOW_WIDTH / GRID_SIZE);
-
+		uint32_t index = (player.position.x / GRID_SIZE) + (player.position.y / GRID_SIZE) * ((float)WINDOW_WIDTH / GRID_SIZE);
 		grid_target[index] = 1;
 
-		DrawRectangleV((Vector2){ x, y }, player.size, RED);
+		DrawRectangleV((Vector2){ player.position.x, player.position.y }, player.size, RED);
 		DrawRectangleLinesEx((Rectangle){
-								 .x = x,
-								 .y = y,
+								 .x = player.position.x,
+								 .y = player.position.y,
 								 .width = player.size.x,
 								 .height = player.size.y },
 			2, BLACK);
@@ -77,4 +84,31 @@ int main() {
 
 	CloseWindow();
 	return 0;
+}
+
+Vector2 to_vec2(int32_t direction) {
+	return (Vector2){
+		.x = direction == 2 ? 1 : direction == 4 ? -1
+												 : 0,
+		.y = direction == 1 ? -1 : direction == 3 ? 1
+												  : 0,
+	};
+}
+
+void print_player(Player p) {
+	printf("Player {\n  position { x = %.2f, y = %.2f }\n}\n",
+		p.position.x, p.position.y);
+}
+
+void draw_grid(uint32_t grid_target[GRID_SIZE * GRID_SIZE]) {
+	uint32_t rows = WINDOW_HEIGHT / GRID_SIZE, columns = WINDOW_WIDTH / GRID_SIZE;
+	static Color colors[5] = { RAYWHITE, RED, BLUE, GREEN, ORANGE };
+
+	for (uint32_t y = 0; y < rows; y++) {
+		for (uint32_t x = 0; x < columns; x++) {
+			uint32_t index = x + y * columns;
+			DrawRectangle(x * GRID_SIZE, y * GRID_SIZE, GRID_SIZE, GRID_SIZE, colors[grid_target[index]]);
+			DrawRectangleLines(x * GRID_SIZE, y * GRID_SIZE, GRID_SIZE, GRID_SIZE, GRAY);
+		}
+	}
 }
